95b5eeb6606460ea9de1c39df7f5c2de98987696
Adithya Hegde Kota <hegdeadithyak@gmail.com>
2024-02-18 14:57:54 +0530

    PlayMusic Version 1

---
+++ PlayMusic.cjs
@@ -0,0 +1,83 @@
+const ytdl = require("discord-ytdl-core");
+const { Client, GatewayIntentBits  } = require("discord.js");
+const { joinVoiceChannel,createAudioPlayer ,createAudioResource} = require('@discordjs/voice');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const client = new Client({
+    intents :[GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages],
+});
+
+const prefix = "!";
+
+client.on("ready", () => {
+    console.log("ready");
+    const data = [
+        {
+            name: 'ping',
+            description: 'Replies with Pong!',
+        },
+        {
+            name: 'echo',
+            description: 'Replies with Echo!',
+        },
+    ];
+    
+    
+});
+
+client.on("interactionCreate", async interaction => { 
+    
+    if (!interaction.isCommand()) return; 
+
+    const commandName = interaction.commandName;
+    console.log(commandName);
+
+    if (commandName === "ping") {
+        await interaction.reply("Pong! <:Pingsock:433019097005948938>"); 
+    } else if (commandName === "avatar") {
+        await interaction.reply({ content: interaction.user.avatarURL() }); 
+    } else if (commandName === "play") {
+        if (!interaction.member.voice.channel) {
+            return await interaction.reply("You're not in a voice channel?"); 
+        }
+
+        // const stream = ytdl(
+        //     "https://www.youtube.com/watch?v=QnL5P0tFkwM",
+        //     {
+        //         filter: "audioonly",
+        //         opusEncoded: true,
+        //         encoderArgs: ["-af", "bass=g=10,dynaudnorm=f=200"],
+        //     }
+        // );  
+        // 
+        //This is a older version which we used might be used later so commented out.
+
+        try {
+            const connection = await joinVoiceChannel({
+                channelId: interaction.member.voice.channel.id,
+                guildId: interaction.guild.id,
+                adapterCreator: interaction.guild.voiceAdapterCreator,
+            });
+
+            const url = interaction.options.getString("url") || "https://youtu.be/QnL5P0tFkwM?si=q9z8By2auwKB3D0n"; 
+
+            const stream = ytdl(url, {
+                filter: "audioonly",
+                opusEncoded: true,
+                encoderArgs: ["-af", "bass=g=10,dynaudnorm=f=200"],
+            });
+
+            const player = createAudioPlayer();
+            const resource = createAudioResource(stream);
+            player.play(resource);
+            connection.subscribe(player);
+
+            await interaction.reply({ content: "Playing the song!" }); 
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: "Error playing the song!" });
+        }
+    }
+});
+
+client.login("MTE3OTg0ODk2MDMyNDY4NTg3NA.GP89-2.qnt65yDFB3JgVn3Xuk2u7uN15kP83Jd7xiQKVY");
--- index.cjs
+++ index.cjs
@@ -1,9 +1,5 @@
-require("dotenv").config();
-require("./modules/checkValid");
-
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
-const { Player } = require("discord-player");
 
 const bot = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
@@ -11,3 +7,35 @@ const bot = new Client({
 
 
 
+const { useMainPlayer } = require('discord-player');
+
+async function execute(interaction) {
+    const player = useMainPlayer();
+    const channel = interaction.member.voice.channel;
+    if (!channel) return interaction.reply('You are not connected to a voice channel!'); 
+    const query = interaction.options.getString('query', true); 
+   
+    await interaction.deferReply();
+
+    try {
+        const { track } = await player.play(channel, query, {
+            nodeOptions: {
+                metadata: interaction
+            }
+        });
+
+        return interaction.followUp(`**${track.title}** enqueued!`);
+    } catch (e) {
+        return interaction.followUp(`Something went wrong: ${e}`);
+    }
+}
+
+bot.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === '!play') {
+        await execute(interaction);
+    }
+});
+
+bot.login("MTE3OTg0ODk2MDMyNDY4NTg3NA.GP89-2.qnt65yDFB3JgVn3Xuk2u7uN15kP83Jd7xiQKVY");
