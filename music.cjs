cf9987385dce8badbac6e1cd0348f97a16d1b56a
Adithya Hegde Kota <hegdeadithyak@gmail.com>
2024-02-24 10:09:18 +0530

    Added play function using song name

--- music.cjs
+++ music.cjs
@@ -4,7 +4,7 @@ const ytdl = require("discord-ytdl-core");
 const { Client, GatewayIntentBits  } = require("discord.js");
 const { joinVoiceChannel,createAudioPlayer ,createAudioResource} = require('@discordjs/voice');
 const { SlashCommandBuilder } = require('discord.js');
-
+const yts = require('yt-search');
 
 
 const client = new Client({
@@ -13,42 +13,24 @@ const client = new Client({
 
 // const prefix = "!";
 
+
+
 client.on("ready", () => {
-    console.log("Logged in as " + client.user.tag);
-    new SlashCommandBuilder()
-    .setName('echo')
-    .setDescription('Echoes your song')
-    .addStringOption(option =>
-        option.setName('input')
-            .setDescription('The input to echo back')
-            .setRequired(true))
-//     client.application.commands.set([
-//         {
-//             name: "ping",
-//             description: "Replies with Pong!",
-//         },
-//         {
-//             name: "avatar",
-//             description: "Replies with your avatar!",
-//         },
-//         {
-//             name: "play",
-//             description: "Plays a song!",
-//             options: [
-//                 {
-//                     name: "url",
-//                     type: "STRING",
-//                     description: "The URL of the song to play",
-//                     required: true,
-//                 },
-//             ],
-//         },
-//     ]).then(console.log)
-//   .catch(console.error);;
+    console.log(`Logged in as ${client.user.tag}!`);
     
+    const data = new SlashCommandBuilder()
+    .setName('sing')
+    .setDescription('Sings your song')
+    .addStringOption(option =>
+        option.setName('song_name')
+            .setDescription('The song name to sing')
+            .setRequired(true))
+    client.application.commands.create(data);
+
 });
 
 client.on("interactionCreate", async interaction => { 
+    
     
     if (!interaction.isCommand()) return; 
     
@@ -59,34 +41,28 @@ client.on("interactionCreate", async interaction => {
     }
     else if (interaction.commandName === "avatar") {
         await interaction.reply({ content: interaction.user.avatarURL() }); 
-    } else if (interaction.commandName === "echo") {
-        const input = interaction.options.getString('input');
+    } else if (interaction.commandName === "sing") {
+        const input = interaction.options.getString('song_name');
         
         if (!interaction.member.voice.channel) {
             return await interaction.reply("You're not in a voice channel?"); 
         }
-
-        // const stream = ytdl(
-        //     "https://www.youtube.com/watch?v=QnL5P0tFkwM",
-        //     {
-        //         filter: "audioonly",
-        //         opusEncoded: true,
-        //         encoderArgs: ["-af", "bass=g=10,dynaudnorm=f=200"],
-        //     }
-        // );  
-        // 
-        //This is a older version which we used, might be used later so commented out.
-
         try {
             const connection = await joinVoiceChannel({
                 channelId: interaction.member.voice.channel.id,
                 guildId: interaction.guild.id,
                 adapterCreator: interaction.guild.voiceAdapterCreator,
             });
+             
+            const {videos} = await yts(input);
+            if (!videos.length) return message.channel.send("No songs were found!");
+            const song = {
+                title: videos[0].title,
+                url: videos[0].url
+            };
             
-            const url = input.trim(); 
-            console.log(url);
-            const stream = ytdl(url, {
+            // console.log(song);
+            const stream = ytdl(song.url, {
                 filter: "audioonly",
                 opusEncoded: true,
                 encoderArgs: ["-af", "bass=g=10,dynaudnorm=f=200"],
@@ -97,7 +73,7 @@ client.on("interactionCreate", async interaction => {
             player.play(resource);
             connection.subscribe(player);
 
-            await interaction.reply({ content: `Playing the song: ${url}` }); 
+            await interaction.reply({ content: `Playing the song: ${song.url}` }); 
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: "Error playing the song!" });
